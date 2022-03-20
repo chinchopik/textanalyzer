73f3753ac44b60b0e545822cf0790d27d50ffd6e
chinchopik <101990000+chinchopik@users.noreply.github.com>
2022-03-20 20:40:17 +0300

    Add files via upload

---
+++ task/task/Form2.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace task
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.Analyze = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(56, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(320, 200);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // Analyze
+            // 
+            this.Analyze.Location = new System.Drawing.Point(56, 231);
+            this.Analyze.Name = "Analyze";
+            this.Analyze.Size = new System.Drawing.Size(238, 23);
+            this.Analyze.TabIndex = 2;
+            this.Analyze.Text = "Провести анализ";
+            this.Analyze.UseVisualStyleBackColor = true;
+            this.Analyze.Click += new System.EventHandler(this.ButtonAnalyze);
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(300, 231);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(75, 23);
+            this.Back.TabIndex = 3;
+            this.Back.Text = "Назад";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.ButtonBack);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 281);
+            this.Controls.Add(this.Back);
+            this.Controls.Add(this.Analyze);
+            this.Controls.Add(this.richTextBox1);
+            this.MaximumSize = new System.Drawing.Size(450, 320);
+            this.MinimumSize = new System.Drawing.Size(450, 320);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ввод текста вручную";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2FormClosed);
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button Analyze;
+        private System.Windows.Forms.Button Back;
+    }
+}
---
+++ task/task/Form2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace task
+{
+
+
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            MaximizeBox = false;
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void ButtonAnalyze(object sender, EventArgs e)
+        {
+            TextAnalyzer buttonAnalyze = new TextAnalyzer();
+            string text = richTextBox1.Text;
+
+            buttonAnalyze.GetText(text);
+            buttonAnalyze.Print(text);
+        }
+
+        private void ButtonBack(object sender, EventArgs e)
+        {
+            Hide();
+            Form1 buttonBack = new Form1();
+            buttonBack.Show();
+        }
+
+        public class TextAnalyzer
+    {
+            public int vowels, consonants, numbers, punctuation, symbols;
+            public void GetText(string text)
+            { 
+                vowels = Regex.Matches(text, @"[аеоуиэюыёя]", RegexOptions.IgnoreCase).Count;
+                consonants = Regex.Matches(text, @"[йцкнгшщзхфвпрлджчсмтб]", RegexOptions.IgnoreCase).Count;
+                numbers = Regex.Matches(text, @"[0-9]").Count;
+                punctuation = Regex.Matches(text, @"[.,:;!?)(–-]").Count;
+                symbols = vowels + consonants + numbers + punctuation;
+            }
+            
+
+            public void Print(string text)
+            {
+                if (text != null)
+                {
+                    MessageBox.Show($"Количество гласных: {vowels}\nКоличество согласных: {consonants}\nКоличество цифр: {numbers}\nКоличество пункуационных знаков: {punctuation}\nКоличество символов: {symbols}", "Всплывающее окно");
+                }
+            }
+}
+
+        private void Form2FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
